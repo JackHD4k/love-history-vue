2c585b7c09b652abb6c4432740940dad0364fa4a
joelramos <joelramos456@gmail.com>
2022-07-28 16:08:33 -0500

    header

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Textbox from './components/Textbox.vue';
+</script>
+
+<template>
+  <header>
+    <h1 class="title-header">Nuestra Historia</h1>
+  </header>
+  <Textbox />
+</template>
+
+<style lang="postcss" scoped>
+header {
+  text-align: center;
+  border-style: solid none solid none;
+  border-width: 1px;
+  margin: 30px;
+  .title-header {
+    font-style: italic;
+    padding: 10px;
+  }
+}
+</style>
---
+++ src/components/Textbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <p>Costa verde</p>
+      <p>17/07/2022</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
